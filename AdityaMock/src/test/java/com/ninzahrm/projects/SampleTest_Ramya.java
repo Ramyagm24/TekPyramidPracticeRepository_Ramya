1189512d6909381883763efd52580fa0b9774591
Sandeepramya <sandeepramya01062023@outlook.com>
2025-11-06 16:25:06 +0530

    Create SampleTest_Ramya Testcase

---
+++ AdityaMock/src/test/java/com/ninzahrm/projects/SampleTest_Ramya.java
@@ -0,0 +1,12 @@
+package com.ninzahrm.projects;
+
+public class SampleTest_Ramya {
+
+	public static void main(String[] args) {
+		System.out.println("create contact");
+		System.out.println("create contact with orgname");
+		System.out.println("create contact with phonenumber");
+
+	}
+
+}
